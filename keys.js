10f24aeae7ce9445a9419e9a6ce20d11ba4c16ad
Gabitec <anagubi@gmail.com>
2019-09-25 13:57:56 -0400

    pushtoGIT

---
+++ keys.js
@@ -0,0 +1,10 @@
+// console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.spotifyId,
+    secret: process.env.spotifySecret,
+};
+
+exports.oK = process.env.omdbKey;
+exports.bITK = process.env.bandsInTownKey;
+
